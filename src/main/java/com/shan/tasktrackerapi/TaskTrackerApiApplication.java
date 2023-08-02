27945e9ffdd8e7ca5ca954bde61b521c8a2bb992
Shan <shanpiv@gmail.com>
2023-08-02 04:25:57 +0300

    Starts project

---
+++ src/main/java/com/shan/tasktrackerapi/TaskTrackerApiApplication.java
@@ -0,0 +1,15 @@
+package com.shan.tasktrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerApiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TaskTrackerApiApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/shan/tasktrackerapi/TaskTrackerApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.shan.tasktrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+class TaskTrackerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
